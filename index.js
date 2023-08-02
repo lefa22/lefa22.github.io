bb9453f10f5095f74f111c6c45a9007e011a9224
lefa22 <66741521+lefa22@users.noreply.github.com>
2023-08-02 23:03:56 +0200

    updated click animation + animation for heading

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let hoveredElement = null;
 let more = 0;
 let mouseX = 0;
 let mouseY = -150;
+let letters = Array.from(document.getElementsByClassName("headerSpan"));
 
 window.onpointermove = event => {
   const { clientX, clientY } = event;
@@ -15,7 +16,7 @@ window.onpointermove = event => {
 
 
 window.onmousedown = event => {
-    more = 10;
+    more = 30;
 }
 
 window.onmouseup = event => {
@@ -33,7 +34,6 @@ function updateCursor() {
      onButton = false
     }
  }
- console.log(more)
 
  bgCursor.animate({
    left: `${mouseX}px`,
@@ -63,3 +63,28 @@ function updateCursor() {
 }
 
 setInterval(updateCursor, 50)
+
+let calledOnce = false;
+
+function mouseOnHeader(status) {
+    if (status && !calledOnce) {
+      letters.forEach(letter => {
+        letter.animate({
+          left: `${Math.random()*50-25}px`,
+          top: `${Math.random()*50-25}px`
+        }, { duration: 400, fill: "forwards"});
+        console.log(letter)
+        calledOnce = true;
+    });
+  }
+    else if (!status) {
+      letters.forEach(letter => {
+      letter.animate({
+        left: `0px`,
+        top: `0px`
+      }, { duration: 400, fill: "forwards" });
+      console.log(letter)
+      calledOnce = false;
+  });
+}
+}
