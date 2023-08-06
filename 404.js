e9f500c317dad40b484ffc6f6af7bbc85e52d80e
lefa22 <lefa22@xyz.de>
2023-08-06 13:29:56 +0200

    added hover effect to 404 screen heading

--- 404.js
+++ 404.js
@@ -1,4 +1,5 @@
 address = window.location.toString();
+let heading = document.getElementById("heading");
 
 document.getElementById("windowLocation").innerText = address;
 
@@ -42,4 +43,21 @@ window.onpointermove = event => {
       height: `${rect.height + 10}px`
     }, { duration: 400, fill: "forwards" });
   }
+}
+
+function mouseOnHeading() {
+    for (let i = 0; i < 3; i++) {
+        for (let i2 = 0; i2 < 10; i2++) {
+            if (i == 0) {
+                setTimeout(() => {heading.innerHTML = `${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`}, (i*10+i2)*40);
+            }
+            else if (i == 1) {
+              setTimeout(() => {heading.innerHTML = `4${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`}, (i*10+i2)*40);
+            }
+            else if (i == 2) {
+              setTimeout(() => {heading.innerHTML = `40${Math.floor(Math.random()*10)}`}, (i*10+i2)*40);
+            }
+        }
+    }
+    setTimeout(() => {heading.innerHTML = "404";}, 30*40);
 }
