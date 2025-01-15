c1bd8268f9c17b90a31734efc196b32b04054705
lefa22 <66741521+lefa22@users.noreply.github.com>
2025-01-15 11:45:31 +0100

    added analytics + minor changes

---
+++ analytics.js
@@ -0,0 +1,80 @@
+const url = "https://data.lefa22.com/items/homepage_analytics";
+
+const data = {
+    path: "",
+    referer: "",
+    browser: "",
+    os: "",
+    "mobile": false,
+    resolution: "",
+    language: ""
+};
+
+let platform = "";
+let browser = "";
+let mobile = false;
+
+
+
+function locationType(){
+    if( window.location.protocol == 'file:' ){ return 0; }
+    if( !window.location.host.replace( /localhost|127\.0\.0\.1/i, '' ) ){ return 2; }
+    return 1;
+}
+
+function checkOS(n) {
+    if (n.userAgentData) {
+        const hints = ["architecture", "model", "platform", "platformVersion", "uaFullVersion"];
+        n.userAgentData.getHighEntropyValues(hints)
+            .then(ua => {
+                platform = `${ua.platform} ${ua.architecture}`;
+                browser = `${ua.brands[0].brand} ${ua.brands[0].version}`;
+                mobile = ua.mobile;
+
+                fillData();
+                //console.log(data);
+                if (locationType() == 2) {
+                    sendData();
+                }
+            });
+    } else {
+        console.log(n.userAgent);
+        return "navigator.userAgentData is not supported!";
+    }
+}
+
+async function sendData() {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": ""
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        console.log("Data sent successfully:", response.status);
+    } catch (error) {
+        console.error("Error sending data:", error);
+    }
+}
+
+function fillData() {
+    data.path = window.location.pathname;
+    data.referer = document.referrer;
+    data.browser = browser;
+    data.os = platform;
+    data.mobile = mobile;
+    data.resolution = `${window.screen.width}x${window.screen.height}`;
+    data.language = navigator.language;
+}
+
+
+
+checkOS(navigator);
+
--- global.js
+++ global.js
@@ -16,21 +16,26 @@ function getCookie(name) {
     return null;
 }
 
-function switch_mode(element, dont_switch=false) {
+function switch_mode(element, dont_switch=false, noCookie=false) {
     if (!dont_switch) {
         element.innerHTML = modes[(modes.indexOf(element.innerHTML) + 1) % modes.length];
     }
     if (element.innerHTML === "night_sight_max") {
+        document.body.classList.remove("light-mode");
         document.body.classList.remove("dark-mode");
         document.body.classList.add("oled-mode");
     } else if (element.innerHTML === "dark_mode") {
+        document.body.classList.remove("oled-mode");
         document.body.classList.remove("light-mode");
         document.body.classList.add("dark-mode");
     } else {
+        document.body.classList.remove("dark-mode");
         document.body.classList.remove("oled-mode");
         document.body.classList.add("light-mode");
     }
-    setCookie('theme', element.innerHTML, 365); // Saves for 1 year
+    if (!noCookie) {
+        setCookie('theme', element.innerHTML, 365); // Saves for 1 year
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -42,6 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
         switch_mode(themeSwitcher, true);
     } else if (!prefersDark) {
         themeSwitcher.innerHTML = "light_mode";
-        switch_mode(themeSwitcher, true);
+        switch_mode(themeSwitcher, true, true);
     }
 });
